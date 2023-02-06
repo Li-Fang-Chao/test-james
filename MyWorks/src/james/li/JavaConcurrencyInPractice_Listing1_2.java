a2fe6c472f3aad4ed1e169d0306393dd91b54140
James <james.fangchao.li@kingland.com>
2023-02-06 11:03:04 +0800

    List 1.1, List 1.2

---
+++ MyWorks/src/james/li/JavaConcurrencyInPractice_Listing1_2.java
@@ -0,0 +1,60 @@
+package james.li;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 
+ * @author jamli
+ *
+ */
+public class JavaConcurrencyInPractice_Listing1_2 {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		Sequence newSequence = new Sequence();
+		
+		List<Integer> values = Collections.synchronizedList(new ArrayList<Integer>());
+		CountDownLatch latch = new CountDownLatch(3);
+
+		
+		Runnable countDown = () -> {
+			int nextValue = 0;
+			while(nextValue < 2000) {
+				nextValue = newSequence.getNext();
+				values.add(nextValue);
+			}
+			latch.countDown();
+		};
+
+		ExecutorService executorService = Executors.newFixedThreadPool(3);
+		executorService.execute(countDown);
+		executorService.execute(countDown);
+		executorService.execute(countDown);
+		executorService.shutdown();
+		
+		latch.await();
+		
+		/**
+		 * Checking if the list contains duplicate value. If it contains a duplicate, it means the result is not thread safe
+		 */
+		boolean hasDuplicate = values.size() != new HashSet<Integer>(values).size();
+		System.out.println("Has duplicate values? " + hasDuplicate);
+
+	}
+
+}
+
+class Sequence {
+	
+	private int value;
+
+	public synchronized int getNext() {
+		return value++;
+	}
+}
