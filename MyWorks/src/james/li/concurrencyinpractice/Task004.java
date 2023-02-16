ad327604cbbc5f5f5b4e1f5ff69c60ab9a687efc
James <james.fangchao.li@kingland.com>
2023-02-16 22:24:16 +0800

    thread safe map

---
+++ MyWorks/src/james/li/concurrencyinpractice/Task004.java
@@ -0,0 +1,93 @@
+package james.li.concurrencyinpractice;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 
+ * @author jamli
+ *
+ */
+public class Task004 {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		Runnable runnable = () -> {
+
+		};
+
+	}
+
+}
+
+class ThreadSafeMapDecorator implements Map<Object, Object> {
+
+	private Map<Object, Object> innerMap = new HashMap<Object, Object>();
+
+	public ThreadSafeMapDecorator(Map<Object, Object> map) {
+		this.innerMap = map;
+	}
+
+	@Override
+	public int size() {
+		return innerMap.size();
+	}
+
+	@Override
+	public synchronized boolean isEmpty() {
+		return innerMap.isEmpty();
+	}
+
+	@Override
+	public synchronized boolean containsKey(Object key) {
+		return innerMap.containsKey(key);
+	}
+
+	@Override
+	public synchronized boolean containsValue(Object value) {
+		return innerMap.containsValue(value);
+	}
+
+	@Override
+	public synchronized Object get(Object key) {
+		return innerMap.get(key);
+	}
+
+	@Override
+	public synchronized Object put(Object key, Object value) {
+		return innerMap.put(key, value);
+	}
+
+	@Override
+	public synchronized Object remove(Object key) {
+		return innerMap.remove(key);
+	}
+
+	@Override
+	public synchronized void putAll(Map m) {
+		innerMap.putAll(m);
+	}
+
+	@Override
+	public synchronized void clear() {
+		innerMap.clear();
+	}
+
+	@Override
+	public synchronized Set keySet() {
+		return innerMap.keySet();
+	}
+
+	@Override
+	public synchronized Collection values() {
+		return innerMap.values();
+	}
+
+	@Override
+	public synchronized Set entrySet() {
+		return innerMap.entrySet();
+	}
+
+}
