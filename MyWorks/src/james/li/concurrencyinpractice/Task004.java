e180192a8d4c0a8c7908817fbd5dca9599974dc5
James <james.fangchao.li@kingland.com>
2023-03-06 08:39:41 +0800

    new a map each time

--- MyWorks/src/james/li/concurrencyinpractice/Task004.java
+++ MyWorks/src/james/li/concurrencyinpractice/Task004.java
@@ -21,11 +21,9 @@ public class Task004 {
 		System.out.println("Running 100 job with ImprovedMap " + tester.timeTasks(100, addValuesToMap(new ImprovedMap(new HashMap()))));
 		System.out.println("Running 1000 job with ImprovedMap " + tester.timeTasks(1000, addValuesToMap(new ImprovedMap(new HashMap()))));
 		
-		Map anotherMap = new ConcurrentHashMap();
-		
-		System.out.println("Running 10 job with ConcurrentHashMap " + tester.timeTasks(10, addValuesToMap(anotherMap)));
-		System.out.println("Running 100 job with ConcurrentHashMap " + tester.timeTasks(100, addValuesToMap(anotherMap)));
-		System.out.println("Running 1000 job with ConcurrentHashMap " + tester.timeTasks(1000, addValuesToMap(anotherMap)));
+		System.out.println("Running 10 job with ConcurrentHashMap " + tester.timeTasks(10, addValuesToMap(new ConcurrentHashMap())));
+		System.out.println("Running 100 job with ConcurrentHashMap " + tester.timeTasks(100, addValuesToMap(new ConcurrentHashMap())));
+		System.out.println("Running 1000 job with ConcurrentHashMap " + tester.timeTasks(1000, addValuesToMap(new ConcurrentHashMap())));
 	}
 
 	/**
